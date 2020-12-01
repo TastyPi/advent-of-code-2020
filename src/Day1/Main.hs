13b3f7ec618af8b91dd98eab999933b42accb5cb
Graham Rogers <graham@rogers.me.uk>
2020-12-01 22:16:58 +0000

    Add solution for day 1 part 2

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,18 +1,38 @@
-{-# LANGUAGE TupleSections, TypeApplications #-}
+{-# LANGUAGE LambdaCase, TupleSections, TypeApplications #-}
 module Day1.Main where
 
+import Control.Arrow
+import Control.Monad
+import Data.Profunctor.Product.Flatten
 import System.IO
 
-pairs :: [a] -> [(a, a)]
+pairs :: [a] -> [[a]]
 pairs [] = []
-pairs (x:xs) = map (x,) xs ++ pairs xs
+pairs (x:xs) = map (\y -> [x, y]) xs ++ pairs xs
 
-part1 :: IO Int
-part1 = withFile "data/Day1/input.txt" ReadMode $ \handle -> do
-  input <- hGetContents handle
-  case filter (\(x, y) -> x + y == 2020) $ pairs $ map read $ lines input of
-    [(x, y)] -> return $ x * y
-    x -> error $ "Failed to find single pair: " ++ show x
+triples :: [a] -> [[a]]
+triples [] = []
+triples (x:xs) = map (x:) (pairs xs) ++ triples xs
+
+onlyElement :: [a] -> a
+onlyElement [x] = x
+onlyElement _ = error $ "List does not have a single element"
+
+findSolution :: (Eq a, Num a) => [[a]] -> a
+findSolution = product . onlyElement . filter ((== 2020) . sum)
+
+computePart1 :: (Eq a, Num a) => [a] -> a
+computePart1 = findSolution . pairs
+
+computePart2 :: (Eq a, Num a) => [a] -> a
+computePart2 = findSolution . triples
+
+type Entry = Int
+
+withInput :: ([Entry] -> IO a) -> IO a
+withInput f = withFile "data/Day1/input.txt" ReadMode $  (f . map read . lines) <=< hGetContents
 
 main :: IO ()
-main = part1 >>= print
+main = withInput $ \input -> do
+  putStrLn $ "Part 1: " ++ show (computePart1 input)
+  putStrLn $ "Part 2: " ++ show (computePart2 input)
