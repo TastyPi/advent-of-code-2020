10d84f324eeaddaaec32b02a03d6a3dbe3325cf1
Graham Rogers <graham@rogers.me.uk>
2020-12-05 03:19:01 +0000

    Day 4 part 1 solution
    I'm not particularly pleased wit hthis, there's a lot of repetition.

--- advent2020/Advent2020.hs
+++ advent2020/Advent2020.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Advent2020 (withInputLines, withParsedInputLines) where
+module Advent2020
+  ( withInputLines,
+    withParsedInput,
+    withParsedInputLines,
+  )
+where
 
+import Control.Arrow ((>>>))
 import Control.Monad ((>=>))
 import Data.Attoparsec.Text (Parser, parseOnly)
+import Data.Attoparsec.Text.Lazy (Result (Done, Fail))
+import qualified Data.Attoparsec.Text.Lazy as Lazy
 import Data.Text (Text)
-import Data.Text.Lazy (lines, toStrict)
+import Data.Text.Lazy (lines, toStrict, unpack)
+import qualified Data.Text.Lazy as T.Lazy
 import Data.Text.Lazy.IO (hGetContents)
-import System.IO (BufferMode (LineBuffering), Handle, IOMode (ReadMode), hSetBuffering, withFile)
+import System.IO
+  ( BufferMode (LineBuffering),
+    Handle,
+    IOMode (ReadMode),
+    hSetBuffering,
+    withFile,
+  )
 
 withInputLines :: String -> ([Text] -> IO a) -> IO a
 withInputLines file evaluate = withFile file ReadMode $ readLines >=> evaluate
@@ -20,3 +35,13 @@ readLines :: Handle -> IO [Text]
 readLines handle =
   hSetBuffering handle LineBuffering
     >> map toStrict . Data.Text.Lazy.lines <$> hGetContents handle
+
+withParsedInput :: String -> Lazy.Parser a -> (a -> IO b) -> IO b
+withParsedInput file parser evaluate =
+  withFile file ReadMode $
+    hGetContents >=> Lazy.parse parser >>> \case
+      Fail _ _ msg -> error msg
+      Done remaining result ->
+        if T.Lazy.null remaining
+          then evaluate result
+          else error ("Didn't parse everything:\n" ++ unpack remaining)
--- day4/Main.hs
+++ day4/Main.hs
@@ -1,4 +1,86 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
+import Advent2020 (withParsedInput)
+import Control.Applicative (Alternative ((<|>)))
+import Control.Lens (ASetter, makeFields, set, (^.))
+import Data.Attoparsec.Text (Parser, char, sepBy, sepBy1, space, string)
+import qualified Data.Attoparsec.Text as A
+import Data.Char (isSpace)
+import Data.Functor (($>))
+import Data.Maybe (mapMaybe)
+import Data.Text (Text)
+
+data PassportInput = PassportInput
+  { _passportInputByr :: Maybe Text,
+    _passportInputIyr :: Maybe Text,
+    _passportInputEyr :: Maybe Text,
+    _passportInputHgt :: Maybe Text,
+    _passportInputHcl :: Maybe Text,
+    _passportInputEcl :: Maybe Text,
+    _passportInputPid :: Maybe Text,
+    _passportInputCid :: Maybe Text
+  }
+
+data Passport = Passport
+  { _passportByr :: Text,
+    _passportIyr :: Text,
+    _passportEyr :: Text,
+    _passportHgt :: Text,
+    _passportHcl :: Text,
+    _passportEcl :: Text,
+    _passportPid :: Text,
+    _passportCid :: Maybe Text
+  }
+
+makeFields ''PassportInput
+makeFields ''Passport
+
+keyValue :: ASetter s t a b -> Text -> Parser b -> Parser (s -> t)
+keyValue setter k v = set <$> (string k $> setter) <*> (char ':' *> v)
+
+passportInputKeyValue :: Parser (PassportInput -> PassportInput)
+passportInputKeyValue =
+  keyValue byr "byr" simpleValue
+    <|> keyValue iyr "iyr" simpleValue
+    <|> keyValue eyr "eyr" simpleValue
+    <|> keyValue hgt "hgt" simpleValue
+    <|> keyValue hcl "hcl" simpleValue
+    <|> keyValue ecl "ecl" simpleValue
+    <|> keyValue pid "pid" simpleValue
+    <|> keyValue cid "cid" simpleValue
+  where
+    simpleValue = pure <$> A.takeTill isSpace
+
+maybePassport :: Parser PassportInput
+maybePassport =
+  foldl
+    (flip id)
+    (PassportInput Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing)
+    <$> passportInputKeyValue `sepBy1` space
+
+maybePassports :: Parser [PassportInput]
+maybePassports = maybePassport `sepBy` string "\n\n"
+
+validatePassport :: PassportInput -> Maybe Passport
+validatePassport p =
+  Passport
+    <$> (p ^. byr)
+    <*> (p ^. iyr)
+    <*> (p ^. eyr)
+    <*> (p ^. hgt)
+    <*> (p ^. hcl)
+    <*> (p ^. ecl)
+    <*> (p ^. pid)
+    <*> pure (p ^. cid)
+
+validPassports :: Parser [Passport]
+validPassports = mapMaybe validatePassport <$> maybePassports
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = withParsedInput "data/day4/input.txt" validPassports $ \passports -> do
+  putStrLn $ "Part 1: " ++ show (length passports)
