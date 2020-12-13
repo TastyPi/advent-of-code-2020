6f90008ab97dee860bd659f7802f94e4682cac4c
Graham Rogers <graham@rogers.me.uk>
2020-12-13 03:23:05 +0000

    Day 6 part 1 solution

--- day6/Main.hs
+++ day6/Main.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Advent2020 (withInput)
+import Data.Set (fromList)
+import Data.Text.Lazy (unpack)
+import qualified Data.Text.Lazy as T
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = withInput "data/day6/input.txt" $ \input -> do
+  let groups = map T.lines $ T.splitOn "\n\n" input
+  putStrLn $ "Part 1: " ++ show (sum $ map (length . foldMap (fromList . unpack)) groups)
