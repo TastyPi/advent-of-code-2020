a02d8365058c0f34c1ba4db1d75c380485b97a76
Graham Rogers <graham@rogers.me.uk>
2020-12-13 03:28:52 +0000

    Day 6 part 2 solution

--- day6/Main.hs
+++ day6/Main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Advent2020 (withInput)
-import Data.Set (fromList)
+import Data.Set (fromList, intersection)
 import Data.Text.Lazy (unpack)
 import qualified Data.Text.Lazy as T
 
@@ -11,3 +11,4 @@ main :: IO ()
 main = withInput "data/day6/input.txt" $ \input -> do
   let groups = map T.lines $ T.splitOn "\n\n" input
   putStrLn $ "Part 1: " ++ show (sum $ map (length . foldMap (fromList . unpack)) groups)
+  putStrLn $ "Part 2: " ++ show (sum $ map (length . foldr1 intersection . map (fromList . unpack)) groups)
